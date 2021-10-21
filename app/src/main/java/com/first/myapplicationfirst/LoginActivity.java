5436e0c983240608891e058747607a599a6792db
am3dam2 <rodrifdt17@gmail.com>
2021-10-21 09:59:54 +0200

    ventana de login

--- app/src/main/java/com/first/myapplicationfirst/LoginActivity.java
+++ app/src/main/java/com/first/myapplicationfirst/LoginActivity.java
@@ -1,8 +1,9 @@
 package com.first.myapplicationfirst;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * @author Rodrigo
@@ -14,5 +15,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onClick(View v){
+        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(intent);
     }
 }

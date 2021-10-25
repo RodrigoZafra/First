ff5b695a0f8602f1b1dd3c57f168f3c0dfa90dc6
am3dam2 <rodrifdt17@gmail.com>
2021-10-25 12:54:42 +0200

    ventana de login

--- app/src/main/java/com/first/myapplicationfirst/LoginActivity.java
+++ app/src/main/java/com/first/myapplicationfirst/LoginActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 /**
  * @author Rodrigo
@@ -15,10 +19,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this).load(R.drawable.girl).transition(DrawableTransitionOptions.withCrossFade(500)).centerCrop().into(mGirl);
     }
 
-    public void onClick(View v){
+    public void openMain(View v){
         Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
+
+    public void openSignup(View v){
+        Intent intent = new Intent(LoginActivity.this,Signup.class);
+        startActivity(intent);
+    }
+
+
 }

fa9d2b8e2c2c3486869de5b07847d4a13ba48243
am3dam2 <rodrifdt17@gmail.com>
2021-11-02 09:17:01 +0100

    ventana de login

--- app/src/main/java/com/first/myapplicationfirst/SplashScreen.java
+++ app/src/main/java/com/first/myapplicationfirst/SplashScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
@@ -12,12 +14,25 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        openApp(true);
     }
 
-    public void openLogin(View v){
+    /*public void openLogin(View v){
         Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
+    }*/
+    //Esto sirve para pasar de la pantalla de splash a la de login de manera automática
+    private void openApp(boolean locationPermission){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        },2000);
     }
 }

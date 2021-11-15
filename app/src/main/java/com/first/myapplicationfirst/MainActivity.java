0793b4620d3b44b8a7a0e292b199ce50da5f3964
am3dam2 <rodrifdt17@gmail.com>
2021-11-15 12:29:14 +0100

    menu contextual

--- app/src/main/java/com/first/myapplicationfirst/MainActivity.java
+++ app/src/main/java/com/first/myapplicationfirst/MainActivity.java
@@ -1,8 +1,15 @@
 package com.first.myapplicationfirst;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myContext = findViewById(R.id.textTap);
+        registerForContextMenu(myContext);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast toast = Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.item2:
+                Toast toast2 = Toast.makeText(this, "Downloading item...", Toast.LENGTH_LONG);
+                toast2.show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }

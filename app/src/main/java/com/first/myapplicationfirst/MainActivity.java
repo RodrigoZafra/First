f446f25dc32efb0181296395170499a00406dbb9
am3dam2 <rodrifdt17@gmail.com>
2021-11-16 08:54:16 +0100

    tab bar

--- app/src/main/java/com/first/myapplicationfirst/MainActivity.java
+++ app/src/main/java/com/first/myapplicationfirst/MainActivity.java
@@ -2,14 +2,18 @@ package com.first.myapplicationfirst;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,8 +35,18 @@ public class MainActivity extends AppCompatActivity {
     public boolean onContextItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.item1:
-                Toast toast = Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
-                toast.show();
+                // Toast toast = Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+                //toast.show();
+                final ConstraintLayout mLayout = findViewById(R.id.myMainConstraint);
+                Snackbar snackbar = Snackbar.make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+                snackbar.show();
                 return true;
             case R.id.item2:
                 Toast toast2 = Toast.makeText(this, "Downloading item...", Toast.LENGTH_LONG);
@@ -41,5 +55,25 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onContextItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {//Aparece el menu en la barra de arriba
+        getMenuInflater().inflate(R.menu.menu_appbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {//Gestiona las acciones sobre los botones
+        int id = item.getItemId();
+        if(id == R.id.search){
+            Toast toast = Toast.makeText(this,"infecting",Toast.LENGTH_LONG);
+            toast.show();
+        }
+        if(id==R.id.favorite){
+            Toast toast = Toast.makeText(this,"Me gusta!",Toast.LENGTH_LONG);
+            toast.show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

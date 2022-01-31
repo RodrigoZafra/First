f92d7bb57c0078e60ac61b3538d2cdf950792ffb
am3dam2 <rodrifdt17@gmail.com>
2022-01-31 11:46:16 +0100

    nuevo gif

---
+++ app/src/main/java/com/first/myapplicationfirst/SegundoTabbed.java
@@ -0,0 +1,45 @@
+package com.first.myapplicationfirst;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.first.myapplicationfirst.ui.main.SectionsPagerAdapter;
+import com.first.myapplicationfirst.databinding.ActivitySegundoTabbedBinding;
+
+public class SegundoTabbed extends AppCompatActivity {
+
+    private ActivitySegundoTabbedBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySegundoTabbedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
